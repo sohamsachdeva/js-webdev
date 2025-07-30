aa29b18cead1441517cf0f8c529a67fe1d8862c4
sohamsachdeva <sohamsachdeva11@gmail.com>
2025-07-30 17:19:28 +0000

    03_conversionOperators

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,50 @@
+let score = "123abc"
+console.log(typeof score);
+
+let valueNumber = Number(score)
+console.log(valueNumber);
+console.log(typeof valueNumber);
+
+let test = null
+console.log(typeof test);
+
+// "33" => 33
+// "33abc" => NaN => Not a number
+// true = 1, false = 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
+console.log(isLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(typeof someNumber);
+
+// // **********************OPERATIONS********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%3);
+
+str1 = "Hello";
+str2 = " World";
+
+str3 = str1 + str2
+
+console.log(str3);
+// We can just simply add two fucking strings to make it a whole string.
+
+let num = 100;
+num++;
+console.log(num);
+++num;
+console.log(num);
