22c4573dae9d9cbd1bda46f03639925465157f6f
sohamsachdeva <sohamsachdeva11@gmail.com>
2025-08-03 17:50:16 +0000

    05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "soham"
+const repoCount = 1
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('soham-ss-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "   soham    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://soham.com/soham%20sachdeva"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('soham'))
+
+console.log(gameName.split('-'));
